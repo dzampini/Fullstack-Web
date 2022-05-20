3e5f5378c5c2cde405c6397a84ed910000e0dc7a
dj zampini <damianzampini@gmail.com>
2022-05-20 20:11:50 -0300

    modificaciones crud

---
+++ models/login_sucessModel.js
@@ -0,0 +1,62 @@
+const async = require('hbs/lib/async');
+var pool = require('./bd');
+
+
+
+async function getRegistro() {
+    
+        var query = 'SELECT * from registros';
+        var rows = await pool.query(query);
+        return rows;
+        
+}
+
+async function insertRegistro(obj) {
+        try {
+                var query = "insert into registros set ?";
+                var rows = await pool.query(query, [obj]);
+                console.log(obj)
+                return rows;
+        } catch (errors) {
+                console.log(error);
+                throw error;
+                
+        }
+}
+
+async function deleteRegistro(id) {
+        var query = 'delete from registros where id=?';
+        var rows = await pool.query(query, [id]);
+        return rows; 
+}
+ 
+async function insertRegistro(obj) {
+        try {
+                var query = "insert into registros set ?";
+                var rows = await pool.query(query, [obj]);
+                console.log(obj)
+                return rows;
+        } catch (errors) {
+                console.log(error);
+                throw error;
+
+        }
+}
+
+async function getRegistroid(id) {
+
+        var query = "SELECT * from registros where id=?";
+        var rows = await pool.query(query,[id]);
+        return rows[0];
+
+}
+async function modRegistro(obj, id) {
+        try {
+                var query = "UPDATE registros SET ? WHERE id= ?";
+                var rows = await pool.query(query, [obj, id]);
+                return rows;
+        } catch (error) {
+                throw error;
+        }
+ }
+module.exports = {getRegistro, insertRegistro, deleteRegistro, getRegistroid, modRegistro}
---
+++ routes/admin/login_sucess.js
@@ -0,0 +1,109 @@
+var express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+var router = express.Router();
+var registroModel = require('../../models/login_sucessModel');
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+
+    var registro = await registroModel.getRegistro();
+
+    res.render('admin/login_sucess', {
+        layout: 'admin/layout',
+        usuario: req.body.user,
+        registro
+
+
+    })
+    console.log(registro);
+});
+
+router.get('/agregar', (req, res, next) => {
+
+    res.render('admin/agregar', {
+        layout: 'admin/layout',
+
+
+    })
+
+});
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        console.log(req.body);
+
+        if (req.body.nombre != "" && req.body.apellido != "" && req.body.mail != "") {
+            await registroModel.insertRegistro(req.body);
+            res.redirect('/admin/login_sucess')
+
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'todos los campos son requeridos'
+            })
+        }
+
+    } catch (error) {
+
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: "No se cargo el registro"
+        })
+    }
+})
+
+router.get('/eliminar/:id', async (req, res, next) => {
+
+    var id = req.params.id;
+    console.log(id);
+    await registroModel.deleteRegistro(id);
+    res.redirect('/admin/login_sucess');
+
+
+
+});
+
+router.get('/modificar/:id', async (req, res, next) => {
+
+    var id = req.params.id;
+    console.log(id);
+    var obj = await registroModel.getRegistroid(id);
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        obj
+
+    });
+
+});
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+
+        var obj = {
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            mail: req.body.mail,
+        }
+        console.log(obj)
+        await registroModel.modRegistro(obj, req.body.id);
+
+        res.redirect('/admin/login_sucess');
+    }
+
+    catch (error) {
+
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: "No se modifico el registro"
+        })
+    }
+})
+
+module.exports = router;
+
